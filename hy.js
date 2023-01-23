46d52650095d8d800ce88d56c3afc19fe2158c3f
mcnslt <122683984+mcxio@users.noreply.github.com>
2023-01-23 13:08:48 +0800

    Create hy.js

---
+++ hy.js
@@ -0,0 +1,23 @@
+    	function change(){
+
+			document.getElementById("content").innerHTML='<div style="margin:0 auto;border:0px solid;width:340px;height:70px;transform:translate(0px,0px);" id="content"><button class="oo-9"><p style="transform:translate(-9px,0px);color:grey;width:200px;font-size:13px">能解锁低员带会员动听音乐</p><h6 style="transform:translate(6px,-9px);color:#1e9fff;font-size:9px;width:70px;">库存剩余1388</h6></button></div>';
+
+		};
+
+		function change1(){
+
+			document.getElementById("content").innerHTML='<div style="margin:0 auto;border:0px solid;width:340px;height:70px;transform:translate(0px,0px);" id="content"><button class="oo-9"><p style="transform:translate(-9px,0px);color:grey;width:200px;font-size:13px">解锁高低员带会员动听音乐</p><h6 style="transform:translate(6px,-9px);color:#1e9fff;font-size:9px;width:70px;">库存剩余1400</h6></button></div>';
+
+		};
+
+		function change2(){
+
+			document.getElementById("content").innerHTML='<div style="margin:0 auto;border:0px solid;width:340px;height:70px;transform:translate(0px,0px);" id="content"><button class="oo-9"><p style="transform:translate(-9px,0px);color:grey;width:200px;font-size:13px">解锁低员高员带专属会员头</p><h6 style="transform:translate(6px,-9px);color:#1e9fff;font-size:9px;width:70px;">库存剩余1573</h6></button></div>';
+
+		};
+
+		function change3(){
+
+			document.getElementById("content").innerHTML='<div style="margin:0 auto;border:0px solid;width:340px;height:70px;transform:translate(0px,0px);"><button id="myBtn1" class="oo-9"><p style="transform:translate(-9px,0px);color:grey;width:200px;font-size:13px">解低员高员满员带顶级会员</p><h6 style="transform:translate(6px,-9px);color:#1e9fff;font-size:9px;width:70px;">库存剩余1582</h6></button></div>';
+
+		}的
