a434a73268fd2ee07d4d8d373b2ecc9db33252d0
mcnslt <122683984+mcxio@users.noreply.github.com>
2023-01-26 14:51:48 +0800

    Create ass.js

---
+++ ass.js
@@ -0,0 +1,125 @@
+        let put1 = "XJ_Q13856";
+
+        let put2 = "GJ_Q16761";
+
+        let put3 = "MJ_A23943";
+
+        let put4 = "DJ_A56986";
+
+        let count = 0;
+
+        function km1(){
+
+            let pwd1 = document.getElementById("pwd1").value;
+
+            if(pwd1 == put1){
+
+                mui.alert("已解锁会员","充值提示");  
+
+			    document.getElementById("io-yy").innerHTML='<div id="io-yy" class="m3-2" style="transform:translate(0px,-40px);"> <a href="https://mcxio.github.io/www.mdui.io/cip-1.html"><button id="v46" class="m9-2"> <i class="mdui-icon material-icons" style="font-size: 35px; color: #1E9FFF;transform:translate(0px,0px);">hearing</i> </button><p id="v345"></p> <label id="v48" class="label-2" style="color:#F9A825;font-size:12px">低会员音乐</label></a> <p id="v49"> ￴ ￴￴ ￴ ￴ ￴ ￴￴ ￴</p>  <a><button id="v46" class="m9-2"> <i class="mdui-icon material-icons" style="font-size: 35px; color: #1E9FFF;transform:translate(0px,0px);">hearing</i> </button><p id="v345"></p> <label id="v48" class="label-2" style="color:#F9A825;font-size:12px">高会员音乐</label></a> <p id="v49"></div>';            }  else{
+
+                count++;
+
+                if(count>=10){
+
+                    mui.alert("输入次数过多，请稍后再试！","充值提示");  
+
+                }else{
+
+                    mui.alert("卡密无效!","充值提示");  
+
+                }
+
+            }
+
+        };
+
+        function km2(){
+
+            let pwd2 = document.getElementById("pwd2").value;
+
+            if(pwd2 == put2){
+
+                mui.alert("已解锁会员","充值提示");  
+
+                document.getElementById("io-yy").innerHTML='<div id="io-yy" class="m3-2" style="transform:translate(0px,-40px);"> <a href="https://mcxio.github.io/www.mdui.io/cip-2.html"><button id="v46" class="m9-2"> <i class="mdui-icon material-icons" style="font-size: 35px; color: #1E9FFF;transform:translate(0px,0px);">hearing</i> </button><p id="v345"></p> <label id="v48" class="label-2" style="color:#F9A825;font-size:12px">低会员音乐</label></a> <p id="v49"> ￴ ￴￴ ￴ ￴ ￴ ￴￴ ￴</p>    <a href="https://mcxio.github.io/www.mdui.io/cip-1.html"><button id="v46" class="m9-2"> <i class="mdui-icon material-icons" style="font-size: 35px; color: #1E9FFF;transform:translate(0px,0px);">hearing</i> </button><p id="v345"></p> <label id="v48" class="label-2" style="color:#F9A825;font-size:12px">高会员音乐</label></a> <p id="v49"></div>';
+
+            }  else{
+
+                count++;
+
+                if(count>=10){
+
+                    mui.alert("输入次数过多，请稍后再试！","充值提示");  
+
+                }else{
+
+                    mui.alert("卡密无效!","充值提示");  
+
+                }
+
+            }
+
+        };
+
+      function km3(){
+
+            let pwd3 = document.getElementById("pwd3").value;
+
+            if(pwd3 == put3){
+
+                mui.alert("已解锁会员","充值提示");  
+
+                document.getElementById("io-yy").innerHTML='<div id="io-yy" class="m3-2" style="transform:translate(0px,-40px);"> <a href="https://mcxio.github.io/www.mdui.io/cip-2.html"><button id="v46" class="m9-2"> <i class="mdui-icon material-icons" style="font-size: 35px; color: #1E9FFF;transform:translate(0px,0px);">hearing</i> </button><p id="v345"></p> <label id="v48" class="label-2" style="color:#F9A825;font-size:12px">低会员音乐</label></a> <p id="v49"> ￴ ￴￴ ￴ ￴ ￴ ￴￴ ￴</p>    <a href="https://mcxio.github.io/www.mdui.io/cip-1.html"><button id="v46" class="m9-2"> <i class="mdui-icon material-icons" style="font-size: 35px; color: #1E9FFF;transform:translate(0px,0px);">hearing</i> </button><p id="v345"></p> <label id="v48" class="label-2" style="color:#F9A825;font-size:12px">高会员音乐</label></a> <p id="v49"></div>';
+
+	       		document.getElementById("io-dx").innerHTML='<div id="v267" class="c6-5"> <li id="v268" class="m1-5"> <br id="v269"> <label id="v270"> ￴￴ ￴ <a id="v271" onclick="u()" style="color:#F9A825" class="mdui-ripple mdui-bottom-nav-active"> <img id="v272" src=".com/img/.png/01/vo/.com/.oi/op/.gj/miho.png" width="80" style="line-height:5;border-radius:50%;transform:translate(25px,0px);">￴ ￴￴ ￴ ￴ ￴￴ ￴ ￴ ￴ ￴   ￴ ￴ ￴ ￴￴ ￴小草神 </a><a id="v273" href="http://pv.sohu.com/cityjson?ie=utf-8" style="color:#F9A825;"></a> <br id="v274"> <div id="v275" class="he-5"> <p id="v276" style="font-size:10px;transform:translate(38px,6px);width:150px;color:#F9A825">￴GJ号：520520</p>  </div> </label></li> </div> ';
+
+           }  else{
+
+                count++;
+
+                if(count>=10){
+
+                    mui.alert("输入次数过多，请稍后再试！","充值提示");  
+
+                }else{
+
+                    mui.alert("卡密无效!","充值提示");  
+
+                }
+
+            }
+
+        };
+
+      function km4(){
+
+            let pwd4 = document.getElementById("pwd4").value;
+
+            if(pwd4 == put4){
+
+                mui.alert("已解锁会员","充值提示");  
+
+                document.getElementById("io-yy").innerHTML='<div id="io-yy" class="m3-2" style="transform:translate(0px,-40px);"> <a href="https://mcxio.github.io/www.mdui.io/cip-2.html"><button id="v46" class="m9-2"> <i class="mdui-icon material-icons" style="font-size: 35px; color: #1E9FFF;transform:translate(0px,0px);">hearing</i> </button><p id="v345"></p> <label id="v48" class="label-2" style="color:#F9A825;font-size:12px">低会员音乐</label></a> <p id="v49"> ￴ ￴￴ ￴ ￴ ￴ ￴￴ ￴</p>    <a href="https://mcxio.github.io/www.mdui.io/cip-1.html"><button id="v46" class="m9-2"> <i class="mdui-icon material-icons" style="font-size: 35px; color: #1E9FFF;transform:translate(0px,0px);">hearing</i> </button><p id="v345"></p> <label id="v48" class="label-2" style="color:#F9A825;font-size:12px">高会员音乐</label></a> <p id="v49"></div>';
+
+	         	document.getElementById("io-dx").innerHTML='<div id="v267" class="c6-5"> <li id="v268" class="m1-5"> <br id="v269"> <label id="v270"> ￴￴ ￴ <a id="v271" onclick="u()" style="color:#F9A825" class="mdui-ripple mdui-bottom-nav-active"> <img id="v272" src=".com/img/.png/01/vo/.com/.oi/op/.gj/miho.png" width="80" style="line-height:5;border-radius:50%;transform:translate(25px,0px);">￴ ￴￴ ￴ ￴ ￴￴ ￴ ￴ ￴ ￴   ￴ ￴ ￴ ￴￴ ￴小草神 </a><a id="v273" href="http://pv.sohu.com/cityjson?ie=utf-8" style="color:#F9A825;"></a> <br id="v274"> <div id="v275" class="he-5"> <p id="v276" style="font-size:10px;transform:translate(38px,6px);width:150px;color:#F9A825">￴GJ号：520520</p>  </div> </label></li> </div> ';
+
+                document.getElementById("io-ts").innerHTML='<div class="m7-5" style="text-align:center;color:#2196F3;background-color:#EDE7F6"><br>   <div id="playingMusic" style="background-color:#EDE7F6">暂时无记录</div> <br id="v292">  </li> </div>';
+
+            }  else{
+
+                count++;
+
+                if(count>=10){
+
+                    mui.alert("输入次数过多，请稍后再试！","充值提示");  
+
+                }else{
+
+                    mui.alert("卡密无效!","充值提示");  
+
+                }
+
+            }
+
+        };
