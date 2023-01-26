3fc63bd6589dda4fdb8ddc40a9ee7ecc6b1b365f
mcnslt <122683984+mcxio@users.noreply.github.com>
2023-01-26 14:48:19 +0800

    Create dl.js

---
+++ dl.js
@@ -0,0 +1,93 @@
+function u(){
+
+   document.getElementById("ppp").innerHTML=`
+
+<div id="popup" style="position: fixed; top: 0; bottom: 0; left: 0; right: 0; background-color: rgba(0, 0, 0, 0.5); z-index: 999;">
+
+    <div style="position: absolute; top: 50%; left: 50%; transform: translate(-50%, -65%); width: 350px; height: 400px; background-color: #ffffff; padding: 10px;border-radius:30px;">
+
+        <button onclick="document.getElementById('popup').style.display='none'" style="border:0px;font-size:16px;background-color:#fff;transform:translate(267px,10px);">✕</button>
+
+   <div style="text-align:center;">  
+
+    <h3 style="transform:translate(0px,-8px);font-size:30px;height:10px;">注册登录</h3>
+
+    <br>
+
+    <br>
+
+         <p> 昵称 <input id="dem3" class="iutnn" style="color:#000;width:220px;height:26px;border:0px solid;-moz-border-radius:25px;text-shadow:0px 0px 0px White;font-size:15px;" placeholder="  输入昵称,例如朱雨辰" type="text"> </p>
+
+ <p> 邮箱 <input id="demo2" class="iutnn" style="color:#000;width:220px;height:26px;border:0px solid;-moz-border-radius:25px;text-shadow:0px 0px 0px White;font-size:15px;" placeholder="  输入邮箱,例如xxx@qq.com" type="text"> </p>
+
+ 
+
+ <p> 密码 <input id="demo" class="iutnn" style="color:#000;width:220px;height:26px;border:0px solid;-moz-border-radius:25px;text-shadow:0px 0px 0px White;font-size:15px;" placeholder="  输入密码,例如xxxxxx" type="password"> 
+
+  <br>
+
+  <br>
+
+  <button width="" class="bunn" id="v6" onclick="myFunction()">￴￴￴￴注册与登录</button>
+
+  <br>
+
+  
+
+  <p style="color:grey;font-size:12px;">其他登录方式</p>
+
+  <img src="./img/qq.png" width="26px"/>
+
+  </div>
+
+    </div>
+
+</div>`;
+
+}function u(){
+
+   document.getElementById("ppp").innerHTML=`
+
+<div id="popup" style="position: fixed; top: 0; bottom: 0; left: 0; right: 0; background-color: rgba(0, 0, 0, 0.5); z-index: 999;">
+
+    <div style="position: absolute; top: 50%; left: 50%; transform: translate(-50%, -65%); width: 350px; height: 400px; background-color: #ffffff; padding: 10px;border-radius:30px;">
+
+        <button onclick="document.getElementById('popup').style.display='none'" style="border:0px;font-size:16px;background-color:#fff;transform:translate(267px,10px);">✕</button>
+
+   <div style="text-align:center;">  
+
+    <h3 style="transform:translate(0px,-8px);font-size:30px;height:10px;">注册登录</h3>
+
+    <br>
+
+    <br>
+
+         <p> 昵称 <input id="dem3" class="iutnn" style="color:#000;width:220px;height:26px;border:0px solid;-moz-border-radius:25px;text-shadow:0px 0px 0px White;font-size:15px;" placeholder="  输入昵称,例如朱雨辰" type="text"> </p>
+
+ <p> 邮箱 <input id="demo2" class="iutnn" style="color:#000;width:220px;height:26px;border:0px solid;-moz-border-radius:25px;text-shadow:0px 0px 0px White;font-size:15px;" placeholder="  输入邮箱,例如xxx@qq.com" type="text"> </p>
+
+ 
+
+ <p> 密码 <input id="demo" class="iutnn" style="color:#000;width:220px;height:26px;border:0px solid;-moz-border-radius:25px;text-shadow:0px 0px 0px White;font-size:15px;" placeholder="  输入密码,例如xxxxxx" type="password"> 
+
+  <br>
+
+  <br>
+
+  <button width="" class="bunn" id="v6" onclick="myFunction()">￴￴￴￴注册与登录</button>
+
+  <br>
+
+  
+
+  <p style="color:grey;font-size:12px;">其他登录方式</p>
+
+  <img src="./img/qq.png" width="26px"/>
+
+  </div>
+
+    </div>
+
+</div>`;
+
+}
