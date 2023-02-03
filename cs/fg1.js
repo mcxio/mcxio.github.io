a4730c363b63c15afcef48acdf68e486efe5c45d
mcnslt <122683984+mcxio@users.noreply.github.com>
2023-02-03 12:25:20 +0800

    Add files via upload

---
+++ cs/fg1.js
@@ -0,0 +1,50 @@
+    	function fg1(){
+
+			document.getElementById("io-01").innerHTML=`
+       
+ <style>
+ .m-1{
+   background-color:#fff;
+   }
+ .m3-1{
+   background-color:#fff;
+   }
+ <style>
+   `;
+   document.getElementById("io-02").innerHTML=`
+   <style>
+   #f{
+     background-color:#fff;
+   }
+ </style>
+   `;
+  document.getElementById("io-dx").innerHTML='<div id="v267" class="c6-5"> <li id="v268" class="m1-5"> <br id="v269"> <label id="v270"> ￴￴ ￴ <a id="v271" onclick="u()" style="color:grey" class="mdui-ripple mdui-bottom-nav-active"> <img id="v272" src="./img/05.png" width="80" style="line-height:5;border-radius:50%;transform:translate(25px,0px);">￴ ￴￴ ￴ ￴ ￴￴ ￴ ￴ ￴ ￴   ￴ ￴ ￴ ￴￴ ￴注册登录 </a><a id="v273" href="http://pv.sohu.com/cityjson?ie=utf-8" style="color:#F9A825;"></a> <br id="v274"> <div id="v275" class="he-5"> <p id="v276" style="font-size:10px;transform:translate(38px,6px);width:150px;color:grey">￴GJ号：520520</p>  </div> </label></li> </div> ';
+  document.getElementById("io-ts").innerHTML='<div class="m7-5" style="text-align:center;color:#000;background-color:#eee"><br>   <div id="playingMusic" style="background-color:#eee">暂时无记录</div> <br id="v292">  </li> </div>';
+		}
+    	function fg2(){
+
+			document.getElementById("io-01").innerHTML=`
+       
+ <style>
+ .m-1{
+   background-color:#E3F2FD;
+   border:0px solid;
+   box-shadow:0 0px 3px rgba(0,0,0,0.1);
+   }
+ .m3-1{
+   background-color:#E3F2FD;
+   border:0px solid;
+   box-shadow:0 0px 3px rgba(0,0,0,0.2);
+   }
+ <style>
+   `;
+   document.getElementById("io-02").innerHTML=`
+   <style>
+   #f{
+     background-color:#E3F2FD;
+   }
+ </style>
+   `;
+  document.getElementById("io-dx").innerHTML='<div id="v267" class="c6-5"> <li id="v268" class="m1-5" style="box-shadow:0 0px 3px rgba(0,0,0,0.2);background-color:#E3F2FD;"> <br id="v269"> <label id="v270"> ￴￴ ￴ <a id="v271" onclick="u()" style="color:grey" class="mdui-ripple mdui-bottom-nav-active"> <img id="v272" src="./img/05.png" width="80" style="line-height:5;border-radius:50%;transform:translate(25px,0px);">￴ ￴￴ ￴ ￴ ￴￴ ￴ ￴ ￴ ￴   ￴ ￴ ￴ ￴￴ ￴注册登录 </a><a id="v273" href="http://pv.sohu.com/cityjson?ie=utf-8" style="color:#F9A825;"></a> <br id="v274"> <div id="v275" class="he-5" style="background-color:#E3F2FD"> <p id="v276" style="font-size:10px;transform:translate(38px,6px);width:150px;color:grey;">￴GJ号：520520</p>  </div> </label></li> </div> ';
+  document.getElementById("io-ts").innerHTML='<div class="m7-5" style="text-align:center;color:#000;background-color:#E3F2FD"><br>   <div id="playingMusic" style="background-color:#E3F2FD">暂时无记录</div> <br id="v292">  </li> </div>';
+		}
