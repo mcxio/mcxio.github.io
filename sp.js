e021500e5999d4b2d7cd13cd04381b18c65a090a
mcnslt <122683984+mcxio@users.noreply.github.com>
2023-02-11 19:34:13 +0800

    Update sp.js

--- sp.js
+++ sp.js
@@ -1,1 +1,14 @@
 
+  document.getElementById("ip1").innerHTML=`
+
+ <a href="https://nsmclt.github.io/bf1.html">
+
+	<div style="background: url('./img/1.jpg');transform:translate(14px,0px);border-radius:3px;box-shadow:0 0px 1px rgba(0,0,0,0.4);width:150px;height:80px;background-size:100%">t</div>  <p style="transform:translate(174px,-75px);font-size:16px;width:190px;color:#000;">开发者自述软件发展历程</p>
+
+  <p style="transform:translate(174px,-60px);font-size:12px;color:#616161;width:190px">0浏览量 · 2023-02-11</p>
+
+ </div></a>
+
+ `;
+
+ 
