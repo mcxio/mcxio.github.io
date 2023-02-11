5106e58f5846e69a61deef3fa0597161e2d07766
mcnslt <122683984+mcxio@users.noreply.github.com>
2023-02-11 16:27:20 +0800

    Create go.js

---
+++ go.js
@@ -0,0 +1,11 @@
+document.getElementById("go").innerHTML = `
+
+ <a href="bf1.html">
+
+	<div style="background: url('./img/1.jpg');transform:translate(14px,0px);border-radius:3px;box-shadow:0 0px 1px rgba(0,0,0,0.4);width:150px;height:80px;background-size:100%"></div>  <p style="transform:translate(174px,-89px);font-size:16px;width:190px;color:#000;">开发者自述软件发展历程</p>
+
+  <p style="transform:translate(174px,-69px);font-size:12px;color:#616161;width:190px">0浏览量 · 2023-02-11</p>
+
+ </div></a>
+
+</div>`;
