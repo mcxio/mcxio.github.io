a750a5ecd71af3c35dc4462b4706d3662187291c
mcnslt <122683984+mcxio@users.noreply.github.com>
2023-02-11 20:31:29 +0800

    Update ass.js

--- ass.js
+++ ass.js
@@ -6,6 +6,8 @@
 
         let put4 = "DJ_A56986";
 
+        var put5 = ["LB_5231","LB_3735","LB_3439"];
+
         let count = 0;
 
         function km1(){
@@ -105,6 +107,33 @@
 	         	document.getElementById("io-dx").innerHTML='<div id="v267" class="c6-5"> <li id="v268" class="m1-5"> <br id="v269"> <label id="v270"> ￴￴ ￴ <a id="v271" onclick="u()" style="color:#F9A825" class="mdui-ripple mdui-bottom-nav-active"> <img id="v272" src=".com/img/.png/01/vo/.com/.oi/op/.gj/miho.png" width="80" style="line-height:5;border-radius:50%;transform:translate(25px,0px);">￴ ￴￴ ￴ ￴ ￴￴ ￴ ￴ ￴ ￴   ￴ ￴ ￴ ￴￴ ￴小草神 </a><a id="v273" href="http://pv.sohu.com/cityjson?ie=utf-8" style="color:#F9A825;"></a> <br id="v274"> <div id="v275" class="he-5"> <p id="v276" style="font-size:10px;transform:translate(38px,6px);width:150px;color:#F9A825">￴GJ号：520520</p>  </div> </label></li> </div> ';
 
                 document.getElementById("io-ts").innerHTML='<div class="m7-5" style="text-align:center;color:#2196F3;background-color:#EDE7F6"><br>   <div id="playingMusic" style="background-color:#EDE7F6">暂时无记录</div> <br id="v292">  </li> </div>';
+
+            }  else{
+
+                count++;
+
+                if(count>=10){
+
+                    mui.alert("输入次数过多，请稍后再试！","充值提示");  
+
+                }else{
+
+                    mui.alert("卡密无效!","充值提示");  
+
+                }
+
+            }
+
+        };
+       function km5(){
+
+            let pwd5 = document.getElementById("pwd5").value;
+
+            if(pwd5 == put5[0] || pwd5 == put5[1] || pwd5 == put5[2]){
+
+                mui.alert("您已解锁会员音乐(全)","提示");  
+
+                document.getElementById("io-yy").innerHTML='<div id="io-yy" class="m3-2" style="transform:translate(0px,-40px);"> <a href="https://mcxio.github.io/www.mdui.io/cip-2.html"><button id="v46" class="m9-2"> <i class="mdui-icon material-icons" style="font-size: 35px; color: #1E9FFF;transform:translate(0px,0px);">hearing</i> </button><p id="v345"></p> <label id="v48" class="label-2" style="color:#F9A825;font-size:12px">低会员音乐</label></a> <p id="v49"> ￴ ￴￴ ￴ ￴ ￴ ￴￴ ￴</p>    <a href="https://mcxio.github.io/www.mdui.io/cip-1.html"><button id="v46" class="m9-2"> <i class="mdui-icon material-icons" style="font-size: 35px; color: #1E9FFF;transform:translate(0px,0px);">hearing</i> </button><p id="v345"></p> <label id="v48" class="label-2" style="color:#F9A825;font-size:12px">高会员音乐</label></a> <p id="v49"></div>';
 
             }  else{
 
