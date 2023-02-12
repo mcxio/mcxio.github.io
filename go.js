29ba448eda7114851112bef060dfe6704b8a5ddb
mcnslt <122683984+mcxio@users.noreply.github.com>
2023-02-12 13:45:58 +0800

    Update go.js

--- go.js
+++ go.js
@@ -1,5 +1,15 @@
 document.getElementById("go").innerHTML = `
 
+ <a href="bf2.html">
+
+	<div style="background: url('./imgsp/2.jpg');transform:translate(14px,0px);border-radius:3px;box-shadow:0 0px 1px rgba(0,0,0,0.4);width:150px;height:80px;background-size:100%"></div>  <p style="transform:translate(174px,-89px);font-size:16px;width:190px;color:#000;">今天我来讲一讲GJ</p>
+
+  <p style="transform:translate(174px,-69px);font-size:12px;color:#616161;width:190px">0浏览量 · 2023-02-12</p>
+
+ </div></a>
+
+ <div style="transform:translate(0px,-74px);">
+
  <a href="bf1.html">
 
 	<div style="background: url('./imgsp/1.jpg');transform:translate(14px,0px);border-radius:3px;box-shadow:0 0px 1px rgba(0,0,0,0.4);width:150px;height:80px;background-size:100%"></div>  <p style="transform:translate(174px,-89px);font-size:16px;width:190px;color:#000;">开发者自述软件发展历程</p>
@@ -9,3 +19,8 @@ document.getElementById("go").innerHTML = `
  </div></a>
 
 </div>`;
+
+</script>
+
+
+
