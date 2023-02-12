9c15914ba28fa978f260264656d3d321a903cc4d
mcnslt <122683984+mcxio@users.noreply.github.com>
2023-02-12 13:55:43 +0800

    Update go.js

--- go.js
+++ go.js
@@ -2,7 +2,7 @@ document.getElementById("go").innerHTML = `
 
  <a href="bf2.html">
 
-	<div style="background: url('https://nsmclt.github.io/imgsp/2.jpg.jpg');transform:translate(14px,0px);border-radius:3px;box-shadow:0 0px 1px rgba(0,0,0,0.4);width:150px;height:80px;background-size:100%"></div>  <p style="transform:translate(174px,-89px);font-size:16px;width:190px;color:#000;">今天我来讲一讲GJ</p>
+	<div style="background: url('https://nsmclt.github.io/imgsp/2.jpg.jpg');transform:translate(14px,0px);border-radius:3px;box-shadow:0 0px 1px rgba(0,0,0,0.4);width:150px;height:80px;background-size:100%"></div>  <p style="transform:translate(174px,-89px);font-size:16px;width:190px;color:#000;">今天我来讲一讲GJ-C</p>
 
   <p style="transform:translate(174px,-69px);font-size:12px;color:#616161;width:190px">0浏览量 · 2023-02-12</p>
 
