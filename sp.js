7bde3b639d741ec40a356d913e93c467e4a15266
mcnslt <122683984+mcxio@users.noreply.github.com>
2023-02-12 14:08:57 +0800

    Update sp.js

--- sp.js
+++ sp.js
@@ -1,6 +1,6 @@
 document.getElementById("ip1").innerHTML=`
 
- <a href="https://nsmclt.github.io/bf1.html">
+ <a href="https://nsmclt.github.io/bf2.html">
 
 	<div style="background: url('https://nsmclt.github.io/imgsp/2.jpg.jpg');transform:translate(14px,0px);border-radius:3px;box-shadow:0 0px 1px rgba(0,0,0,0.4);width:150px;height:80px;background-size:100%"></div>  <p style="transform:translate(174px,-75px);font-size:16px;width:190px;color:#000;">今天我来讲一讲GJ-C</p>  <p style="transform:translate(174px,-60px);font-size:12px;color:#616161;width:190px">0浏览量 · 2023-02-11</p>
 
@@ -8,7 +8,7 @@ document.getElementById("ip1").innerHTML=`
 
   <div style="transform:translate(0px,-74px);">
 
- <a href="bf1.html">
+ <a href="https://nsmclt.github.io/bf1.html">
 
 	<div style="background: url('./imgsp/1.jpg');transform:translate(14px,20px);border-radius:3px;box-shadow:0 0px 1px rgba(0,0,0,0.4);width:150px;height:80px;background-size:100%"></div>  <p style="transform:translate(174px,-55px);font-size:16px;width:190px;color:#000;">开发者自述软件发展历程</p>
 
