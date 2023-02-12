11f74b462c684b7d36189ba07560a016d0de8110
mcnslt <122683984+mcxio@users.noreply.github.com>
2023-02-12 14:34:04 +0800

    Update sp.js

--- sp.js
+++ sp.js
@@ -4,7 +4,7 @@ document.getElementById("ip1").innerHTML=`
 
 	<div style="background: url('https://nsmclt.github.io/imgsp/2.jpg.jpg');transform:translate(14px,0px);border-radius:3px;box-shadow:0 0px 1px rgba(0,0,0,0.4);width:150px;height:80px;background-size:100%"></div>  <p style="transform:translate(174px,-75px);font-size:16px;width:190px;color:#000;">今天我来讲一讲GJ-C</p>  <p style="transform:translate(174px,-60px);font-size:12px;color:#616161;width:190px">0浏览量 · 2023-02-11</p>
 
- </div></a>
+ </a></div>
 
   <div style="transform:translate(0px,-74px);">
 
