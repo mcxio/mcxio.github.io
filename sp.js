48658b5b3a1ae42a2bed510a7133c554d1407d15
mcnslt <122683984+mcxio@users.noreply.github.com>
2023-02-19 18:11:40 +0800

    Update sp.js

--- sp.js
+++ sp.js
@@ -19,3 +19,33 @@ document.getElementById("ip1").innerHTML=`
 </div>
 
  `;
+   document.getElementById("ip1").innerHTML=`
+
+ <a href="https://nsmclt.github.io/bf2.html">
+
+	<div style="background: url('https://nsmclt.github.io/imgsp/2.jpg.jpg');transform:translate(14px,0px);border-radius:3px;box-shadow:0 0px 1px rgba(0,0,0,0.4);width:150px;height:80px;background-size:100%"></div>  <p style="transform:translate(174px,-75px);font-size:16px;width:190px;color:#000;">今天我来讲一讲GJ-C</p>  <p style="transform:translate(174px,-60px);font-size:12px;color:#616161;width:190px">0浏览量 · 2023-02-12</p>
+
+ </a></div>
+
+  <div style="transform:translate(0px,-54px);">
+
+ <a href="https://nsmclt.github.io/bf1.html">
+
+	<div style="background: url('https://nsmclt.github.io/imgsp/1.jpg');transform:translate(14px,20px);border-radius:3px;box-shadow:0 0px 1px rgba(0,0,0,0.4);width:150px;height:80px;background-size:100%"></div>  <p style="transform:translate(174px,-55px);font-size:16px;width:190px;color:#000;">开发者自述软件发展历程</p>
+
+  <p style="transform:translate(174px,-39px);font-size:12px;color:#616161;width:190px">0浏览量 · 2023-02-11</p>
+
+ </div></a>
+
+  <div style="transform:translate(0px,-89px);">
+
+ <a href="https://nsmclt.github.io/bf3.html">
+
+	<div style="background: url('https://nsmclt.github.io/imgsp/2.jpg');transform:translate(14px,20px);border-radius:3px;box-shadow:0 0px 1px rgba(0,0,0,0.4);width:150px;height:80px;background-size:100%"></div>  <p style="transform:translate(174px,-55px);font-size:16px;width:190px;color:#000;">僵尸生存 #1</p>
+
+  <p style="transform:translate(174px,-39px);font-size:12px;color:#616161;width:190px">223856浏览量 · 2023-02-19</p>
+
+ </div></a>
+</div>
+
+ `;
